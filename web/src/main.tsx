4afb46eb458bbde1eba3796286b8f74109275e53
Henrique Cole Fernandes <henriquecolefernandes@gmail.com>
2023-09-15 16:32:05 -0300

    feat: created routes for login and tasks

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,20 @@
+import { App } from '@/App.tsx'
+import { Tasks } from '@/pages/Tasks/'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App.tsx'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+
+      <Routes>
+        <Route path="/tasks" element={<Tasks />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
