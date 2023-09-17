dfb26d22d547ed25057940eb78dc039df56e6d32
HenriqueCole <henriquecolefernandes@gmail.com>
2023-09-17 15:23:16 -0300

    remove: unecessary function

--- web/src/services/sandBox.js
+++ web/src/services/sandBox.js
@@ -1,10 +1,5 @@
 const { post } = require("./crud/firebase");
 
-// async function getTasks(res: any) {
-//   const tasks = await crud.get("tasks");
-//   res.send(tasks);
-// }
-
 async function createTask() {
   const task = {
     description: "test",
