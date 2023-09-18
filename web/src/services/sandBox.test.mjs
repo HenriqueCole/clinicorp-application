3647083a5bc60099ca70cb61db8559730772fc10
Henrique Cole Fernandes <henriquecolefernandes@gmail.com>
2023-09-18 16:11:49 -0300

    add file type

--- web/src/services/sandBox.test.mjs
+++ web/src/services/sandBox.test.mjs
@@ -7,7 +7,7 @@ import {
   getTaskById,
   unblockTask,
   updateTask
-} from './api/tasks/tasks.handler'
+} from './api/tasks/tasks.handler.js'
 
 test('Check if the task creation succeeded', async () => {
   const task = await createTask({
